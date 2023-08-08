f1c722df56a5adb16b2633e310f989d0b5e0a67e
Ptiful <strazzu@gmail.com>
2023-08-08 16:33:12 +0200

    azeazea

--- lecho/lecho  copy.py
+++ lecho/lecho  copy.py
@@ -1,4 +1,5 @@
-import requests
+# import ssl
+# ssl._create_default_https_context = ssl._create_unverified_context
 import requests
 from bs4 import BeautifulSoup as bs
 import pymongo
@@ -11,34 +12,28 @@ import tqdm
 
 load_dotenv()
 
-XML_URL = "https://www.hln.be/sitemap-news.xml"
+XML_URL = "https://www.knack.be/news-sitemap.xml"
 
-JSON_FILEPATH = os.path.join('.', "hln_scrapper","my_json.json")
-
-def fetch_cookie(url, session):
-    """ Will fetch cookie from hln so we can see content"""
-
-    cookie = session.get(url).cookies
-    return cookie
+JSON_FILEPATH = os.path.join('.', "lecho","my_json.json")
 
 def get_links(url, session):
     """will get all links from sitemap"""
-    pages = requests.get(url, cookies= fetch_cookie(url, session)).content
-    soup = bs(pages, "html.parser")
+    page = session.get(url).text
+    soup = bs(page, "html.parser")
     
     for link in soup.find_all("loc"):
         yield link.text
 
 def get_article(link, session):
     container = {}
-    
+
     response = session.get(link)
     if response.status_code!= 200:
-        print(response.status_code) 
+        print(response.status_code)
+        return 
     soup = bs(response.content, "html.parser")
     
-    url = link
-    container["url"] = url
+    container["url"] = link
 
     title = soup.find_all("h1")
     if title :
@@ -47,16 +42,17 @@ def get_article(link, session):
     else :
         print("no title")
 
-    elements = soup.find_all("p")
+    elements = soup.find_all("p", attrs={"class": None})
     if elements :
         text_list = [element.get_text() for element in elements]
         container["article"] = "\n".join(text_list)
     else :
         print("no articles")
 
-    published_time = soup.find("meta", property="article:published_time")
+    published_time = soup.find('script', {"type": "application/ld+json"})
     if published_time:
-        published_date = published_time.get("content", None)
+        data = json.loads(published_time.text, strict=False)
+        published_date = data["@graph"][0]["datePublished"]
         container["date"] = published_date
     else :
         print("no date")
@@ -71,7 +67,6 @@ def get_articles(links, session):
 def save_json(articles):
     """Usefull to see if there is a mistake when scrapping"""
 
-    JSON_FILEPATH = os.path.join('.', "hln_scrapper","my_json.json")
     with open(JSON_FILEPATH, "w") as f:
         json.dump(articles, f)
 
@@ -85,7 +80,7 @@ def mangodb_connection(articles):
     collection = database[collection_name]
 
     for article in tqdm.tqdm(articles):
-        if not collection.find_one({"url": {"$eq": article["url"]}}):
+        if article is not None and not collection.find_one({"url": {"$eq": article["url"]}}):
             collection.insert_one(article)
     # collection.insert_many(articles)
 
@@ -95,7 +90,6 @@ def main():
     to a Mango database to be automatize by Airflow."
     """
     with requests.Session() as session:
-        fetch_cookie("https://www.hln.be", session)
         links = get_links(XML_URL, session)
         articles = get_articles(links, session)
     # save_json(container)
