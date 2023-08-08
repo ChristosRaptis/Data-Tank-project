366c4407251ce38a9a3ed2b517e823fd2aadabf0
Ptiful <strazzu@gmail.com>
2023-08-08 14:52:49 +0200

    thanks Pierre

--- hln_scrapper/hln_articles.py
+++ hln_scrapper/hln_articles.py
@@ -1,97 +1,104 @@
-import functools
 import requests
-import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs
-from tqdm import tqdm
 import pymongo
-from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 import json
+from functools import partial
+from tqdm.contrib.concurrent import thread_map
+import tqdm
 
 load_dotenv()
 
+XML_URL = "https://www.hln.be/sitemap-news.xml"
 
-def fetch_cookie(url, session):  # will fetch cookie from hln so we can see content
+JSON_FILEPATH = os.path.join('.', "hln_scrapper","my_json.json")
+
+def fetch_cookie(url, session):
+    """ Will fetch cookie from hln so we can see content"""
+
     cookie = session.get(url).cookies
     return cookie
 
 def get_links(url, session):
-    url = "https://www.hln.be/sitemap-news.xml"
+    """will get all links from sitemap"""
     pages = requests.get(url, cookies= fetch_cookie(url, session)).content
-
-    # fetch_cookie(url, session)
     soup = bs(pages, "html.parser")
-    links = []
-    all_links = soup.find_all("loc")
+    
+    for link in soup.find_all("loc"):
+        yield link.text
 
-    for link in all_links:
-        links.append(link.text)
-    return links
+def get_article(link, session):
+    container = {}
+    response = session.get(link)
+    if response.status_code!= 200:
+        print(response.status_code) 
+    soup = bs(response.content, "html.parser")
+    
+    url = link
+    container["url"] = url
 
+    title = soup.find_all("h1")
+    if title :
+        article_title = title[0].text.strip()
+        container["title"] = article_title
+    else :
+        print("no title")
 
-def fetch_title_article_date(links, session):  # will fetch the title article using session
-    articles = []
+    elements = soup.find_all("p")
+    if elements :
+        text_list = [element.get_text() for element in elements]
+        container["article"] = "\n".join(text_list)
+    else :
+        print("no articles")
 
-    for i, link in enumerate(links) :
-        empty_dico = {}
-        response = session.get(link)
-        soup = bs(response.content, "html.parser")
-        
-        title = soup.find_all("h1")
-        if title :
-            article_title = title[0].text.strip()
-            empty_dico["title"] = article_title
-        else :
-            print("no title")
+    published_time = soup.find("meta", property="article:published_time")
+    if published_time:
+        published_date = published_time.get("content", None)
+        container["date"] = published_date
+    else :
+        print("no date")
 
-        elements = soup.find_all("p")
-        if elements :
-            text_list = [element.get_text() for element in elements]
-            empty_dico["article"] = "\n".join(text_list)
-        else :
-            print(("no articles"))
+    return container
 
-        published_time = soup.find("meta", property="article:published_time")
-        if published_time:
-            published_date = published_time.get("content", None)
-            empty_dico["date"] = published_date
-        else :
-            print("no date")
-
-        articles.append(empty_dico)
-        print(f"Appending article {i+1} / {len(links)}")
-    return articles
+def get_articles(links, session):
+    """Will fetch all titles, articles, dates, from urls, on at a time
+    and will store it in a list of dictionary."""
+    return list(thread_map(partial(get_article, session=session), links))
 
 def save_json(articles):
-    with open(articles, "w") as f:
-        json.dumps(f)
+    """Usefull to see if there is a mistake when scrapping"""
+
+    JSON_FILEPATH = os.path.join('.', "hln_scrapper","my_json.json")
+    with open(JSON_FILEPATH, "w") as f:
+        json.dump(articles, f)
 
 def mangodb_connection(articles):
-    # Create a connection with MangoDB and upload database
+    """Create a connection with MangoDB and upload database"""
     mongodb_url = os.getenv("MONGODB_URI")
     database_name = "bouman_datatank"
     collection_name = "articles"
-    data_to_insert = df.to_dict(orient="records")
     client = pymongo.MongoClient(mongodb_url)
     database = client[database_name]
     collection = database[collection_name]
-    collection.insert_many(data_to_insert)
+
+    for article in tqdm.tqdm(articles):
+        if not collection.find_one({"url": {"$eq": article["url"]}}):
+            collection.insert_one(article)
+    # collection.insert_many(articles)
 
 def main():
-    """input xml sitemaps to be read as a dataframe, droping and renaming columns,
-    and will use all previous functions to create a database that will be send
+    """input xml sitemaps to be read,
+    will use all previous functions to create a database that will be send
     to a Mango database to be automatize by Airflow."
     """
-
-    url = "https://www.hln.be/sitemap-news.xml"
-    session = requests.Session()
-    fetch_cookie("https://www.hln.be", session)
-    links = get_links(url, session)
-    articles = fetch_title_article_date(links, session)
-    save_json(articles)
-    # mangodb_connection(articles)
+    with requests.Session() as session:
+        fetch_cookie("https://www.hln.be", session)
+        links = get_links(XML_URL, session)
+        articles = get_articles(links, session)
+    # save_json(container)
+    mangodb_connection(articles)
 
 if __name__ == "__main__":
     main()
