1fc5ab6505b0fb17c60018ad03d5bdb20c4f4c8b
Chris Raptis <chris@Chriss-MacBook-Pro.local>
2023-08-08 15:14:20 +0200

    finished with rtbf and lesoir scrapers, database conention included

--- lesoir_articles_scraping/lesoir_scraper.py
+++ lesoir_articles_scraping/lesoir_scraper.py
@@ -3,38 +3,30 @@ Scrape https://www.lesoir.be/18/sections/le-direct to get
 the url, title, text, date and language of each article in the news feed.
 """
 import time
-from datetime import datetime
-import pandas as pd
+from dateutil import parser
 import requests
 from bs4 import BeautifulSoup as bs
 import json
 import unicodedata
 from pymongo import MongoClient
-from tqdm import tqdm
 import ssl  # Use this import if you get a Certificate error in Mac
+import os
+from dotenv import load_dotenv
 
-tqdm.pandas()
+load_dotenv()
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 start_time = time.perf_counter()
 
 
-def get_soup(url: str):
-    response = requests.get(url)
-    soup = bs(response.content, "html.parser")
-    return soup
-
-
-def find_article_title(url: str) -> str:
-    soup = get_soup(url)
+def find_article_title(soup) -> str:
     article_title = soup.find("h1").text
     return article_title
 
 
 # selector for geeko.lesoir.be urls
-def geeko_selector(url: str) -> tuple:
-    soup = get_soup(url)
+def geeko_selector(soup) -> tuple:
     div = soup.find("div", attrs={"class": "post-content-area"})
     paragraphs = div.find_all("p")
     if "Suivez Geeko sur Facebook" in paragraphs[-1].text:
@@ -44,8 +36,7 @@ def geeko_selector(url: str) -> tuple:
 
 
 # selector for sosoir.lesoir.be urls
-def sosoir_selector(url: str) -> tuple:
-    soup = get_soup(url)
+def sosoir_selector(soup) -> tuple:
     h2 = soup.find("h2", attrs={"class": "chapeau"}).text.strip()
     div = soup.find("div", attrs={"id": "artBody"})
     paragraphs = div.find_all("p")
@@ -56,8 +47,7 @@ def sosoir_selector(url: str) -> tuple:
 
 
 # selector for www.lesoir.be and soirmag.lesoir.be urls
-def lesoirmag_selector(url: str) -> tuple:
-    soup = get_soup(url)
+def lesoirmag_selector(soup) -> tuple:
     article = soup.find("article", attrs={"class": "r-article"})
     paragraphs = article.find_all("p")
     if "www.soirmag.be" in paragraphs[-1].text:
@@ -66,69 +56,57 @@ def lesoirmag_selector(url: str) -> tuple:
     return paragraphs, text
 
 
-def find_article_text(url: str) -> str:
+def find_article_text(soup, url: str) -> str:
     if "sosoir" in url:
-        paragraphs, text = sosoir_selector(url)
+        paragraphs, text = sosoir_selector(soup)
     elif "geeko" in url:
-        paragraphs, text = geeko_selector(url)
+        paragraphs, text = geeko_selector(soup)
     else:
-        paragraphs, text = lesoirmag_selector(url)
+        paragraphs, text = lesoirmag_selector(soup)
     for p in paragraphs:
         p_text = unicodedata.normalize("NFKD", p.text.strip())
         text += p_text
     return text
 
 
-def find_published_date(url: str) -> str:
-    soup = get_soup(url)
+def find_published_date(soup) -> str:
     script = soup.find("script", {"type": "application/ld+json"})
     data = json.loads(script.text, strict=False)
     try:
         published_date = data["@graph"][0]["datePublished"]
     except:
         published_date = data["datePublished"]
-    date = datetime.strptime(published_date, "%Y-%m-%d %H:%M:%S%z")     
+    date = parser.parse(published_date)
     return date
 
 
-print("Creating list of urls of news page ...")
-soup = get_soup("https://www.lesoir.be/18/sections/le-direct")
+print("Creating list of articles of news page ...")
+response = requests.get("https://www.lesoir.be/18/sections/le-direct")
+soup = bs(response.content, "html.parser")
 articles = []
 base_url = "https://www.lesoir.be"
 links = soup.select("h3 > a")
 for link in links:
+    print("Adding article ...")
     url = link.get("href")
+    print(" Adding url ...")
     dict = {"url": url if "//" in url else base_url + url}
-    dict['text'] = find_article_text(dict['url'])
-    dict['date'] = find_published_date(dict['url'])
-    dict['title'] = find_article_title(dict['url'])
-    dict['language'] = 'fr'
+    link_response = requests.get(dict["url"])
+    link_soup = bs(link_response.content, "html.parser")
+    print(" Adding article text ...")
+    dict["text"] = find_article_text(link_soup, dict["url"])
+    print(" Adding date ...")
+    dict["date"] = find_published_date(link_soup)
+    print(" Adding title ...")
+    dict["title"] = find_article_title(link_soup)
+    print(" Adding language ...")
+    dict["language"] = "fr"
     articles.append(dict)
-
-# print("Creating dataframe ...")
-# df = pd.DataFrame(urls)
-
-# print("Adding text column to dataframe ...")
-# df["text"] = df["url"].progress_apply(find_article_text)
-
-# print("Adding date column to dataframe ...")
-# df["date"] = df["url"].progress_apply(find_published_date)
-# df["date"] = pd.to_datetime(df["date"])
-
-# print("Adding title column to dataframe ...")
-# df["title"] = df["url"].progress_apply(find_article_title)
-
-# print("Adding language column to dataframe ...")
-# df["language"] = "fr"
-
-# print("Creating dictionary from dataframe ...")
-# articles = df.to_dict(orient="records")
+    print(f"The list has {len(articles)} articles")
+print("List completed.")
 
 print("Connecting to database ...")
-client = MongoClient()
-client = MongoClient(
-    "mongodb://bouman:80um4N!@ec2-15-188-255-64.eu-west-3.compute.amazonaws.com:27017/"
-)
+client = MongoClient(os.getenv("MONGODB_URI"))
 db = client["bouman_datatank"]
 collection = db["articles"]
 
--- rtbf_articles_scraping/rtbf_scraper.py
+++ rtbf_articles_scraping/rtbf_scraper.py
@@ -6,31 +6,27 @@ import time
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs
-import ssl # Use this import only if you get a Certificate error in Mac
+import ssl  # Use this import only if you get a Certificate error in Mac
 from pymongo import MongoClient
-from tqdm import tqdm
+import os
+from dotenv import load_dotenv
 
-tqdm.pandas()
+load_dotenv()
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 start_time = time.perf_counter()
 
 
-
-def find_article_title(url: str) -> str:
-    response = requests.get(url)
-    soup = bs(response.content, "html.parser")
+def find_article_title(soup) -> str:
     try:
         article_title = soup.find("h1").text
     except:
-        article_title = soup.find("h2").text   
+        article_title = soup.find("h2").text
     return article_title
 
 
-def find_article_text(url: str) -> str:
-    response = requests.get(url)
-    soup = bs(response.content, "html.parser")
+def find_article_text(soup) -> str:
     paragraphs = [p.text.strip() for p in soup.find_all("p", attrs={"class": None})]
     article_text = "".join(paragraphs)
     return article_text
@@ -41,22 +37,40 @@ sitemap = pd.read_xml("https://www.rtbf.be/site-map/articles.xml")
 
 print("Removing unecessary columns and keeping loc and lastmod ...")
 df = sitemap.drop(["changefreq", "news", "image"], axis=1)
-df.rename(columns={"loc": "source_url", "lastmod": "date"}, inplace=True)
+df.rename(columns={"loc": "url", "lastmod": "date"}, inplace=True)
 
 print("Adding language column ...")
 df["language"] = "fr"
 
-print("Adding article_title column ...")
-df["article_title"] = df["source_url"].apply(find_article_title)
+print("Creating list of articles ...")
+articles = df.to_dict(orient="records")
 
-print("Adding article_text column ...")
-df["article_text"] = df["source_url"].apply(find_article_text)
+print("Adding title and text to articles ...")
+for index, article in enumerate(articles, start=1):
+    print(f"  Updating article {index} of {len(articles)}")
+    response = requests.get(article["url"])
+    soup = bs(response.content, "html.parser")
+    print("    Adding title ...")
+    article["title"] = find_article_title(soup)
+    print("    Adding text ...")
+    article["text"] = find_article_text(soup)
 
-print(" Rearranging column order ...")
-df = df.loc[:, ["source_url", "article_title", "article_text", "date", "language"]]
+print("Connecting to database ...")
+client = MongoClient(os.getenv("MONGODB_URI"))
+db = client["bouman_datatank"]
+collection = db["articles"]
 
-print("Extracting to rtbf_articles.csv ...")
-df.to_csv("rtbf_articles.csv")
+print("Checking if articles already exist in database, if not adding them ...")
+for article in articles:
+    if collection.find_one({"url": {"$eq": article["url"]}}):
+        print(f"url: {article['url']}")
+        print("An article with this url already exists.")
+    else:
+        print("Adding new article ...")
+        collection.insert_one(article)
+
+print("Closing connection to database ...")
+client.close()
 
 end_time = time.perf_counter()
 print(round(end_time - start_time, 2), "seconds")
