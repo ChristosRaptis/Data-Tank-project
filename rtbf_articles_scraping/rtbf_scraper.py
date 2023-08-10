a1da34d18908f498d01cdc52fa98ca4c4747b615
ChristosRaptis <chrisgraptis@gmail.com>
2023-08-10 11:25:43 +0200

    again

--- rtbf_articles_scraping/rtbf_scraper.py
+++ rtbf_articles_scraping/rtbf_scraper.py
@@ -15,23 +15,10 @@ load_dotenv()
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
-start_time = time.perf_counter()
-
-
->>>>>>> Christos_branch
-def find_article_title(url: str) -> str:
-    response = requests.get(url)
-    soup = bs(response.content, "html.parser")
-=======
 start_time = time.perf_counter()
 
 
 def find_article_title(soup) -> str:
->>>>>>> Christos_branch
     try:
         article_title = soup.find("h1").text
     except:
@@ -45,33 +32,13 @@ def find_article_text(soup) -> str:
     return article_text
 
 
-<<<<<<< HEAD
-# Fetch sitemap
-=======
 print("Creating dataframe from sitemap ...")
->>>>>>> Christos_branch
 sitemap = pd.read_xml("https://www.rtbf.be/site-map/articles.xml")
 
 print("Removing unecessary columns and keeping loc and lastmod ...")
 df = sitemap.drop(["changefreq", "news", "image"], axis=1)
 df.rename(columns={"loc": "url", "lastmod": "date"}, inplace=True)
 
-<<<<<<< HEAD
-# Add 'language' column
-df["language"] = "fr"
-
-# Add 'article_title' column
-df["article_title"] = df["source_url"].apply(find_article_title)
-
-# Add 'article_text' column
-df["article_text"] = df["source_url"].apply(find_article_text)
-
-# Rearange column order
-df = df.loc[:, ["source_url", "article_title", "article_text", "date", "language"]]
-
-# export to csv
-df.to_csv("data/rtbf_articles.csv")
-=======
 print("Adding language column ...")
 df["language"] = "fr"
 
@@ -107,4 +74,3 @@ client.close()
 
 end_time = time.perf_counter()
 print(round(end_time - start_time, 2), "seconds")
->>>>>>> Christos_branch
