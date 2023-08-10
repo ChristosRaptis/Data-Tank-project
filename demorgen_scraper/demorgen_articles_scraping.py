5985c85bc4fe558600423596b07b98d4ab2869e2
Mahmoud Hasan <mr.mahmoudhasan@gmail.com>
2023-08-10 11:26:04 +0200

    fixed variable url in env file

--- demorgen_scraper/demorgen_articles_scraping.py
+++ demorgen_scraper/demorgen_articles_scraping.py
@@ -11,26 +11,36 @@ from dateutil import parser
 import os
 import pymongo
 
-def get_title(soup): # Extract title 
-    return soup.find_all('title')[0].text.split('|')[0].strip()
 
-def get_text(soup): # Extract the article's paragraphs
-    paragraphs = ''
-    p_intro = soup.find_all('p', {"class": "artstyle__intro"}) # article intro
+def get_title(soup) -> str:  # Extract title
+    """function to extract the title of the article"""
+
+    return soup.find_all("title")[0].text.split("|")[0].strip()
+
+
+def get_text(soup) -> str:  # Extract the article's paragraphs
+    """function to extract the article's text"""
+
+    paragraphs = ""
+    p_intro = soup.find_all("p", {"class": "artstyle__intro"})  # article intro
     if p_intro:
-        paragraphs  += p_intro[0].text
-    p_tags = soup.find_all('p', {"class": "artstyle__paragraph"})
+        paragraphs += p_intro[0].text
+    p_tags = soup.find_all("p", {"class": "artstyle__paragraph"})
     for parag in p_tags:
-        paragraphs += parag.text # adding article's paragraphs
+        paragraphs += parag.text  # adding article's paragraphs
     return paragraphs
 
-def get_date(soup): # Extract the published date
-    script = soup.find('script', {"type": "application/ld+json"})
+
+def get_date(soup) -> str:  # Extract the published date
+    """function to extract the date"""
+
+    script = soup.find("script", {"type": "application/ld+json"})
     data = json.loads(script.text, strict=False)
-    return data['@graph'][1]['datePublished']
+    return data["@graph"][1]["datePublished"]
 
 
-def get_links(feeds_url): # Extract links from news feed
+def get_links(feeds_url):  # Extract links from news feed
+    """function to extract the news feed's links"""
     page_divs = requests.get(feeds_url).content
     soup = BeautifulSoup(page_divs, "html.parser")
     links = []
@@ -39,22 +49,26 @@ def get_links(feeds_url): # Extract links from news feed
         links.append(link.text)
     return links
 
-def get_article(url, session):
 
+def get_article(url, session) -> pd:
+    """function to get the whole article's components and returns a
+    pandas's data frame"""
     try:
         page_html = session.get(url).content
         soup = BeautifulSoup(page_html, "html.parser")
-        page_type = soup.find_all('span', {"class": "artstyle__labels__label"})
-        page_blog = soup.find_all('span', {"class": "artstyle__labels__live"})
+        page_type = soup.find_all("span", {"class": "artstyle__labels__label"})
+        page_blog = soup.find_all("span", {"class": "artstyle__labels__live"})
         if page_type:
-            if (page_type[0].text == 'Cartoon'):
-                return 
+            if page_type[0].text == "Cartoon":
+                return
             else:
                 article_title = get_title(soup)
                 article_text = get_text(soup)
                 published_date = parser.parse(get_date(soup))
-                row = [[url,article_text,published_date,article_title,'NL']]
-                df = pd.DataFrame(row,columns = ["url","text","date","title","language"])
+                row = [[url, article_text, published_date, article_title, "NL"]]
+                df = pd.DataFrame(
+                    row, columns=["url", "text", "date", "title", "language"]
+                )
         else:
             if page_blog:
                 return
@@ -62,26 +76,31 @@ def get_article(url, session):
                 article_title = get_title(soup)
                 article_text = get_text(soup)
                 published_date = parser.parse(get_date(soup))
-                row = [[url,article_text,published_date,article_title,'NL']]
-                df = pd.DataFrame(row,columns = ["url", "text","date","title", "language"])    
+                row = [[url, article_text, published_date, article_title, "NL"]]
+                df = pd.DataFrame(
+                    row, columns=["url", "text", "date", "title", "language"]
+                )
         return df
-    
+
     except Exception as e:
         print(type(e))
         print(e)
         return e
 
-if __name__ == "__main__":
 
-    feeds_url = 'https://www.demorgen.be/sitemaps/news.xml'
-    links  = get_links(feeds_url)
+if __name__ == "__main__":
+    feeds_url = "https://www.demorgen.be/sitemaps/news.xml"
+    links = get_links(feeds_url)
 
     with requests.Session() as session:
-        news_feed = pd.concat(df for df in thread_map(partial(get_article, session=session), links) 
-                               if isinstance(df, pd.DataFrame))
-    
-news_feed_dict= news_feed.to_dict('records')
-    
+        news_feed = pd.concat(
+            df
+            for df in thread_map(partial(get_article, session=session), links)
+            if isinstance(df, pd.DataFrame)
+        )
+
+news_feed_dict = news_feed.to_dict("records")
+
 mongodb_url = os.getenv("MONGODB_URL")
 database_name = "bouman_datatank"
 collection_name = "articles"
@@ -90,13 +109,13 @@ database = client[database_name]
 collection = database[collection_name]
 
 for article in news_feed_dict:
-        article_url = article["url"]
-        existing_article = collection.find_one({"url": article_url})
-        if not existing_article:
-            collection.insert_one(article)
+    article_url = article["url"]
+    existing_article = collection.find_one({"url": article_url})
+    if not existing_article:
+        collection.insert_one(article)
 
 client.close()
-#----------------------------
+# ----------------------------
 # mongodb_url = 'mongodb://localhost:27017'
 # database_name = "bouman_datatank"
 # collection_name = "articles"
@@ -111,4 +130,4 @@ client.close()
 #             collection.insert_one(article)
 
 # client.close()
-#------------------------------
+# ------------------------------
